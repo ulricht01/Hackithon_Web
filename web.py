605cb6941982b5b5e4a524cd5c215ab0eb6d8792
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2023-09-22 22:15:18 +0200

    0.1.1

--- web.py
+++ web.py
@@ -2,23 +2,25 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 #----------------- Data ---------------------#
-data = pd.read_csv('zdrojak.csv',nrows=10)
+data = pd.read_csv('zdrojak.csv', nrows=50)
+radky = ["idhod", "hodnota", "sldb_rok", "sldb_datum", "ukaz_txt", "misto_regpobytu_txt", "pohlavi_txt", "uzemi_txt", "uzemi_typ"]
+data = data[radky]
 
-#----------------- Číselníky ----------------#
-
+#px.bar(data, data["sldb_datum"], data["hodnota"])
 
 #----------------- Sidebar ------------------#
 sidebar = st.sidebar
 sidebar.title(":bar_chart: :blue[Filtry]")
 sidebar.caption("Vyberte potřebné filtry pro Vaši práci")
 
-filter1 = sidebar.
+filter1 = sidebar.multiselect("Kraje", [])
+filter2 = sidebar.multiselect
 
 #------------------ Stránka ----------------#
 
-
+st.table(data)
 #folium map. funkce pro python
-
+st.write(px.bar(data, data["sldb_datum"], data["hodnota"]))
 
 
 
