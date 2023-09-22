2ad1d34a639264c765581e6b15008a86f8be7fdc
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2023-09-22 22:30:47 +0200

    0.1.2

--- web.py
+++ web.py
@@ -1,26 +1,34 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-#----------------- Data ---------------------#
-data = pd.read_csv('zdrojak.csv', nrows=50)
-radky = ["idhod", "hodnota", "sldb_rok", "sldb_datum", "ukaz_txt", "misto_regpobytu_txt", "pohlavi_txt", "uzemi_txt", "uzemi_typ"]
-data = data[radky]
+from flask import Flask
 
-#px.bar(data, data["sldb_datum"], data["hodnota"])
+st.set_page_config("Stránka")
 
-#----------------- Sidebar ------------------#
-sidebar = st.sidebar
-sidebar.title(":bar_chart: :blue[Filtry]")
-sidebar.caption("Vyberte potřebné filtry pro Vaši práci")
+app = Flask(__name__)
 
-filter1 = sidebar.multiselect("Kraje", [])
-filter2 = sidebar.multiselect
+@app.route('/run_python_script')
+def run_python_script():
+    #----------------- Data ---------------------#
+    data = pd.read_csv('zdrojak.csv', nrows=50)
+    radky = ["idhod", "hodnota", "sldb_rok", "sldb_datum", "ukaz_txt", "misto_regpobytu_txt", "pohlavi_txt", "uzemi_txt", "uzemi_typ"]
+    data = data[radky]
 
-#------------------ Stránka ----------------#
+    #px.bar(data, data["sldb_datum"], data["hodnota"])
 
-st.table(data)
-#folium map. funkce pro python
-st.write(px.bar(data, data["sldb_datum"], data["hodnota"]))
+    #----------------- Sidebar ------------------#
+    sidebar = st.sidebar
+    sidebar.title(":bar_chart: :blue[Filtry]")
+    sidebar.caption("Vyberte potřebné filtry pro Vaši práci")
+
+    filter1 = sidebar.multiselect("Kraje", [])
+    filter2 = sidebar.multiselect
+
+    #------------------ Stránka ----------------#
+
+    st.table(data)
+    #folium map. funkce pro python
+    st.write(px.bar(data, data["sldb_datum"], data["hodnota"]))
 
 
 
