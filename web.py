d91609e0de8250f7657b3aad67745befaa9d196e
ulricht01 <99655536+ulricht01@users.noreply.github.com>
2023-09-22 22:45:18 +0200

    xxx

--- web.py
+++ web.py
@@ -7,28 +7,26 @@ st.set_page_config("Stránka")
 
 app = Flask(__name__)
 
-@app.route('/run_python_script')
-def run_python_script():
-    #----------------- Data ---------------------#
-    data = pd.read_csv('zdrojak.csv', nrows=50)
-    radky = ["idhod", "hodnota", "sldb_rok", "sldb_datum", "ukaz_txt", "misto_regpobytu_txt", "pohlavi_txt", "uzemi_txt", "uzemi_typ"]
-    data = data[radky]
+#----------------- Data ---------------------#
+data = pd.read_csv('zdrojak.csv', nrows=50)
+radky = ["idhod", "hodnota", "sldb_rok", "sldb_datum", "ukaz_txt", "misto_regpobytu_txt", "pohlavi_txt", "uzemi_txt", "uzemi_typ"]
+data = data[radky]
 
-    #px.bar(data, data["sldb_datum"], data["hodnota"])
+#px.bar(data, data["sldb_datum"], data["hodnota"])
 
-    #----------------- Sidebar ------------------#
-    sidebar = st.sidebar
-    sidebar.title(":bar_chart: :blue[Filtry]")
-    sidebar.caption("Vyberte potřebné filtry pro Vaši práci")
+#----------------- Sidebar ------------------#
+sidebar = st.sidebar
+sidebar.title(":bar_chart: :blue[Filtry]")
+sidebar.caption("Vyberte potřebné filtry pro Vaši práci")
 
-    filter1 = sidebar.multiselect("Kraje", [])
-    filter2 = sidebar.multiselect
+filter1 = sidebar.multiselect("Kraje", data["uzemi_txt"].unique())
+#filter2 = sidebar.multiselect()
 
-    #------------------ Stránka ----------------#
+#------------------ Stránka ----------------#
 
-    st.table(data)
-    #folium map. funkce pro python
-    st.write(px.bar(data, data["sldb_datum"], data["hodnota"]))
+st.table(data)
+#folium map. funkce pro python
+st.write(px.bar(data, data["sldb_datum"], data["hodnota"]))
 
 
 
